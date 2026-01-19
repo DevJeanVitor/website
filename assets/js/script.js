46904da1609ef9960af2dca45341f8fa69a0318a
Jean Vitor <165506589+DevJeanVitor@users.noreply.github.com>
2026-01-19 08:58:21 -0300

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-console.log("Portfólio carregado e pronto.");
-
-document.querySelectorAll('.icon-btn').forEach(button => {
-    button.addEventListener('mouseenter', () => {
-        console.log(`Mouse sobre: ${button.innerText}`);
-    });
-});
